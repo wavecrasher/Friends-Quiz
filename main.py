60d8430a89c92a63d8e327db678bb1b5bf0c1ae5
Hannah Evans <wavecrasherhge@gmail.com>
2020-07-24 20:10:23 +0000

    Added more possible outcomes

--- main.py
+++ main.py
@@ -6,8 +6,16 @@ Characters = {
     "AAAABBB" : "You are Chandler!",
     "AAABBBB" : "You are Ross!",
     "AABBBBB" : "You are Pheobe!",
-    "ABBBBBB" : "You are Pheobe!",
+    "ABBBBBB" : "You are Ross!",
     "ABABABA" : "You are Monica!",
+    "ABBAAAB" : "You are Rachel!",
+    "ABAAAAA" : "You are Ross!",
+    "ABAAABB" : "You are Joey!",
+    "ABAABBB" : "You are Pheobe!",
+    "ABABBBB" : "You are Chandler!",
+    "ABABBAB" : "You are Rachel!", 
+    "AABBBAB" : "You are Joey!",
+    "AAABABB" : "Your are Chandler!", 
     "BBBBBBB" : "You are Rachel!",
     "BAAAAAA" : "You are Joey!",
     "BBAAAAA" : "You are Rachel!",
@@ -15,14 +23,21 @@ Characters = {
     "BBBBAAA" : "You are Pheobe! ",
     "BBBBBAA" : "You are Monica!",
     "BBBBBBA" : "You are Ross!",
-    "BABABAB" : "You are Ross!"
+    "BABABAB" : "You are Ross!",
+    "BAAAABA" : "You are Joey!",
+    "BAAABBA" : "You are Ross!",
+    "BAAABAA" : "You are Chandler!",
+    "BAAABAB" : "You are Ross!",
+    "BAAABBB" : "You are Rachel!",
+    "BAABBBB" : "You are Chandler!",
+    "BABBBBB" : "You are Chandler!",
 }
 
 
 print("Hello there, welcome to 'What is your Friends character?' quiz!")
 
 name = input("What is your name? ")
-print(f"Hi {name} are you ready to play? Y or N ? ")
+answer = input(f"Hi {name} are you ready to play? Y or N ? ")
 print()
 if answer.upper() == "Y":
   print("play game")
@@ -30,22 +45,22 @@ if answer.upper() == "Y":
 
   q2 = input("Are you messing or clean? (Choose A or B) \nA) I'm a slob! \nB) Super clean! ")
 
-  q3 = input("Choose a hobby: (Choose A or B) \nA) Playing guitar in a coffe shop \nB) Dinasours ")
+  q3 = input("Choose a hobby: (Choose A or B) \nA) Playing guitar in a coffee shop \nB) Dinasours ")
 
-  q4 = input("Would you rather: (Choose A or B) \nA) Have Monica be your fitness trainer or \nA) Study Unagi ")
+  q4 = input("Would you rather: (Choose A or B) \nA) Have Monica be your fitness trainer or \nB) Study Unagi ")
 
   q5 = input("Smelly Cats or Cooking? (Choose A or B) \nA) Smelly Cats! \nB) Cooking's my life!  ")
 
   q6 = input("Shopping or Eating? (Choose A or B) \nA) Shopping! \nB) Eating! ")
 
-  q7 = input("Are you and only child or do you have siblings? (Choose A or B) \nA) Imma only! \n Siblings!  ")
+  q7 = input("Are you and only child or do you have siblings? (Choose A or B) \nA) Imma only! \nB) Siblings!  ")
 
   choice = q1 + q2 + q3 + q4 + q5 + q6 + q7
 
   wait = input("Would you like to know your Friends Character?:  ")
 
   if wait.upper() == "Y":
-    print("Your Marvel Character is " + Characters[choice.upper()]) 
+    print("Your Friends Character is " + Characters[choice.upper()]) 
   else:
     print("Well that was a waste...")
 
